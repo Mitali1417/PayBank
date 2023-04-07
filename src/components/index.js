e0177f730cd8ca22c9d5997f2d75adbef371018f
Mitali <mitali9205@gmail.com>
2023-04-07 12:18:13 +0530

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+
+
+export{
+    Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Hero, Testimonials, Footer
+};
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. No, it won't take your position as the driver; it will only take you where you want to go.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Life is easier when you have money. You are fortunate if you possess it.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Most of the time, those who are extremely wealthy work in the financial, trading, and commercial sectors.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "4900+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "150+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$290M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.paybank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.paybank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.paybank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.paybank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.paybank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.paybank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.paybank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.paybank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.paybank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.paybank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.paybank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.paybank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
